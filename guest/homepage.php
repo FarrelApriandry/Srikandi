dd1387555abac49e278de9cea13bb5bb9adbbc41
Muhammad Aris Septanugroho <muhammadaris1945@gmail.com>
2023-10-05 19:29:36 +0700

    Master

---
+++ guest/homepage.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Srikandi -  Homepage</title>
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+</head>
+    <body>
+        <div class="app">
+            <div class="sidebar">
+                <header>
+                    SRIKANDI
+                </header>
+                <div class="sidebar-profile">
+                    <img src="../img/profile.jpg" alt="">
+                    <div class="sidebar-profile-information">
+                        <h3>Kobe Bryant</h3>
+                        <p>Professional Basketball Player</p>
+                        <p class="last-profile-sidebar">LA Lakers - NBA - Superstar</p>
+                    </div>
+                </div>
+                <nav class="sidebar-nav">
+                    <ul>
+                        <li>
+                            <a href="#"><i class="ion-bag"></i> <span>Beranda</span></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="ion-ios-settings"></i> <span class="">Controls</span></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="ion-ios-briefcase-outline"></i> <span class="">Tandatangan Naskah</span></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="ion-ios-analytics-outline"></i> <span class="">Registrasi Naskah Keluar</span></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="ion-ios-paper-outline"></i> <span class="">Template Naskah</span></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="ion-ios-navigate-outline"></i> <span class="">Verifikasi Naskah</span></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="ion-ios-medical-outline"></i> <span class="">Naskah Masuk</span></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="ion-ios-medical-outline"></i> <span class="">Naskah Keluar</span></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="ion-ios-medical-outline"></i> <span class="">Daftar Disposisi</span></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="ion-ios-medical-outline"></i> <span class="">Log Disposisi</span></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="ion-ios-medical-outline"></i> <span class="">Daftar Tembusan</span></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="ion-ios-medical-outline"></i> <span class="">Pengaturan</span></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="ion-ios-medical-outline"></i> <span class="">Berkas</span></a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </body>
+</html>
